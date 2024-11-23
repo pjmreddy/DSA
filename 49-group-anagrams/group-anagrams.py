545bb75938460d051a541a7ea721f87683f09a17
peravali810 <66049053+peravali810@users.noreply.github.com>
2024-11-23 16:19:33 -0600

    Time: 11 ms (79.75%) | Memory: 19.9 MB (58.90%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res=defaultdict(list)
+        for i in strs:
+            sort=''.join(sorted(i))
+            res[sort].append(i)
+        return list(res.values())
