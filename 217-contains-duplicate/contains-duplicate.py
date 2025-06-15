df2dd5aea2194469937421444a1d332d22d421f8
Jaganmohan Reddy Peravali <66049053+pjmreddy@users.noreply.github.com>
2025-06-15 14:08:35 -0500

    Time: 11 ms (89.94%) | Memory: 26 MB (18.45%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,3 +1,16 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(set(nums)) != len(nums)
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(set(nums))==len(nums):
+            return False
+        return True
+        # sol = {}
+        # for i in nums:
+        #     sol[i]=1+sol.get(i,0)
+        #     if sol.get(i)>1:
+        #         return True
+        # return False
+        
